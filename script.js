ab25a22794057c1e7a71b3e4ca65f042caf2fa08
SamuelRguezSiverio <marketing@MacBook-Pro-de-Marketing.local>
2022-12-30 16:19:22 +0000

    Ejercicio 6 - Done

--- script.js
+++ script.js
@@ -178,3 +178,28 @@ function feast(beast, dish) {
 
 console.log(feast('Great Blue Heron', 'Garlic Naan'))
 console.log(feast('Chickadee', 'Chocolate Cake'))
+
+/*
+
+6. Your coworker was supposed to write a simple helper function to capitalize a string (that contains a single word) before they went on vacation.
+
+Unfortunately, they have now left and the code they gave you doesn't work. Fix the helper function they wrote so that it works as intended (i.e. make the first character in the string "word" upper case).
+
+Don't worry about numbers, special characters, or non-string types being passed to the function. The string lengths will be from 1 character up to 10 characters, but will never be empty.
+
+    *** MY PATH ***
+
+    6.1 Identificamos el primer caracter del string utilizando el método charAt(0) donde index = 0 y le pasamos el método .toUpperCase() para devolver el resultado en mayúscula.
+    6.2 Añadimos el método .slice(1) para sumar al primer caracter la continuación del resto del string, por eso usamos index = 1, para que obvie el primer caracter y siga la secuencia de lo escrito.
+
+    *** MY PATH ***
+
+*/
+
+function capitalizeWord(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1)
+  }
+
+  console.log(capitalizeWord('hello world!'))
+
+  
